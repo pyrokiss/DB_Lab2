6bdde72ed6e7cc01e917cfaad75101c46718587a
pyrokiss <44851472+pyrokiss@users.noreply.github.com>
2020-05-14 18:51:32 +0300

    Add files via upload

---
+++ populate_new.sql
@@ -0,0 +1,164 @@
+--Rating(виправив)--
+
+INSERT INTO Rating
+VALUES ('E10+');
+INSERT INTO Rating
+VALUES ('E');
+INSERT INTO Rating
+VALUES ('T');
+
+--Platforms--
+
+INSERT INTO Platforms
+VALUES ('PC');
+INSERT INTO Platforms
+VALUES ('X360');
+INSERT INTO Platforms
+VALUES ('PS4');
+INSERT INTO Platforms
+VALUES ('PS3');
+INSERT INTO Platforms
+VALUES ('3DS');
+
+
+--Genres--
+
+INSERT INTO Genres
+VALUES ('Action');
+INSERT INTO Genres
+VALUES ('Action Adventure');
+INSERT INTO Genres
+VALUES ('Sports');
+INSERT INTO Genres
+VALUES ('Strategy');
+INSERT INTO Genres
+VALUES ('Horror');
+INSERT INTO Genres
+VALUES ('Adventure');
+INSERT INTO Genres
+VALUES ('Role-Playing');
+
+
+--Publishers--
+
+INSERT INTO Publishers
+VALUES ('Ubisoft');
+INSERT INTO Publishers
+VALUES ('Team Cherry');
+INSERT INTO Publishers
+VALUES ('Konami');
+INSERT INTO Publishers
+VALUES ('Nintendo');
+INSERT INTO Publishers
+VALUES ('Capcom');
+INSERT INTO Publishers
+VALUES ('tobyfox');
+INSERT INTO Publishers
+VALUES ('Killmonday Games HB');
+INSERT INTO Publishers
+VALUES ('TheMeatly Games');
+INSERT INTO Publishers
+VALUES ('Matt Makes Games');
+INSERT INTO Publishers
+VALUES ('Tarsier Studios');
+INSERT INTO Publishers
+VALUES ('Moon Studios');
+INSERT INTO Publishers
+VALUES ('Night School Studio');
+INSERT INTO Publishers
+VALUES ('Microsoft Game Studios');
+
+--Developers--
+
+INSERT INTO Developers
+VALUES ('Nintendo');
+INSERT INTO Developers
+VALUES ('Team Cherry');
+INSERT INTO Developers
+VALUES ('Ubisoft');
+INSERT INTO Developers
+VALUES ('Shift');
+INSERT INTO Developers
+VALUES ('Capcom');
+INSERT INTO Developers
+VALUES ('tobyfox');
+INSERT INTO Developers
+VALUES ('Killmonday Games HB');
+INSERT INTO Developers
+VALUES ('TheMeatly Games');
+INSERT INTO Developers
+VALUES ('Matt Makes Games');
+INSERT INTO Developers
+VALUES ('Bandai Namco America');
+INSERT INTO Developers
+VALUES ('Moon Studios');
+INSERT INTO Developers
+VALUES ('Night School Studio');
+
+
+--GamesGenres--
+
+INSERT INTO GamesGenres
+VALUES ('Undertale', 'Role-Playing');
+INSERT INTO GamesGenres
+VALUES ('Hollow knight', 'Action Adventure');
+INSERT INTO GamesGenres
+VALUES ('Fran Bow', 'Adventure');
+INSERT INTO GamesGenres
+VALUES ('Bendy and the ink machine', 'Adventure');
+INSERT INTO GamesGenres
+VALUES ('Grand Theft Auto V', 'Action');
+INSERT INTO GamesGenres
+VALUES ('Grand Theft Auto III', 'Action');
+INSERT INTO GamesGenres
+VALUES ('Celeste', 'Action');
+INSERT INTO GamesGenres
+VALUES ('Little Nightmares', 'Action Adventure');
+INSERT INTO GamesGenres
+VALUES ('Oxenfree', 'Action Adventure');
+INSERT INTO GamesGenres
+VALUES ('Ori and the Blind Forest', 'Action');
+
+
+--GamesPlatforms--
+
+INSERT INTO GamesPlatforms
+VALUES ('Undertale', 'PC');
+INSERT INTO GamesPlatforms
+VALUES ('Hollow knight', 'PC');
+INSERT INTO GamesPlatforms
+VALUES ('Fran Bow', 'PC');
+INSERT INTO GamesPlatforms
+VALUES ('Bendy and the ink machine', 'PS4');
+INSERT INTO GamesPlatforms
+VALUES ('Grand Theft Auto V', 'PS4');
+INSERT INTO GamesPlatforms
+VALUES ('Grand Theft Auto III', 'PC');
+INSERT INTO GamesPlatforms
+VALUES ('Celeste', 'PS4');
+INSERT INTO GamesPlatforms
+VALUES ('Little Nightmares', 'PC');
+INSERT INTO GamesPlatforms
+VALUES ('Oxenfree', 'PC');
+INSERT INTO GamesPlatforms
+VALUES ('Ori and the Blind Forest', 'PC');
+
+
+--Games--
+
+INSERT INTO Games
+VALUES ('Hollow knight','E10+','Team Cherry','Team Cherry','Feb 24.2017');
+INSERT INTO Games
+VALUES ('Undertale','E10+','tobyfox','tobyfox','Sep 15.2015');
+INSERT INTO Games
+VALUES ('Fran Bow','E10+','Killmonday Games HB','Killmonday Games HB','Aug 27.2015');
+INSERT INTO Games
+VALUES ('Bendy and the ink machine','T','TheMeatly Games','TheMeatly Games','Nov 20.2018');
+INSERT INTO Games
+VALUES ('Celeste','E10+','Matt Makes Games','Matt Makes Games','Jan 26.2018');
+INSERT INTO Games
+VALUES ('Little Nightmares','T','Tarsier Studios','Bandai Namco America','Apr 28.2017');
+INSERT INTO Games
+VALUES ('Oxenfree','T','Night School Studio','Night School Studio','Jan 14.2016');
+INSERT INTO Games
+VALUES ('Ori and the Blind Forest','E','Microsoft Game Studios','Moon Studios','Mar 11.2015');    
